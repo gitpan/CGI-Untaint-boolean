02651eed623a9cbd1c3bfc291d103d5f6105b0e5
chromatic <chromatic@wgz.org>
2004-06-26 22:23:19 +0000

    Import of CHROMATIC/CGI-Untaint-boolean-0.10 from CPAN.
    gitpan-cpan-distribution: CGI-Untaint-boolean
    gitpan-cpan-version:      0.10
    gitpan-cpan-path:         CHROMATIC/CGI-Untaint-boolean-0.10.tar.gz
    gitpan-cpan-author:       CHROMATIC
    gitpan-cpan-maturity:     released

---
+++ lib/CGI/Untaint/boolean.pm
@@ -0,0 +1,62 @@
+package CGI::Untaint::boolean;
+
+use strict;
+use vars '$VERSION';
+
+$VERSION = '0.10';
+
+use base 'CGI::Untaint::object';
+
+sub _untaint_re { qr/^(on)$/ }
+
+sub is_valid
+{
+	my $self  = shift;
+	my $value = $self->value();
+
+	return unless $value and $value =~ $self->_untaint_re();
+
+	$self->value( $value eq 'on' ? 1 : 0 );
+	return 1;
+}
+
+1;
+__END__
+
+=head1 NAME
+
+CGI::Untaint::boolean - untaint boolean values from CGI programs
+
+=head1 SYNOPSIS
+
+  use CGI::Untaint;
+
+  my $handler = CGI::Untaint->new( $q->Vars() );
+  my $boolean = $handler->extract( -as_boolean => 'some_feature' );
+
+=head1 DESCRIPTION
+
+This input handler verifies that it is dealing with a reasonable boolean value,
+probably from a checkbox with no value specified.  In this case, "reasonable"
+means that the value is C<on>, if the checkbox is checked, or empty, if the
+client did not send a value.
+
+=head1 SEE ALSO
+
+L<CGI::Untaint>, L<CGI::Untaint::object>
+
+=head1 AUTHOR
+
+chromatic, E<lt>chromatic@wgz.orgE<gt>
+
+Thanks to Tony Bowden for helpful suggestions.
+
+=head1 BUGS
+
+No known bugs.  Please report any to L<http://rt.cpan.org/>.
+
+=head1 COPYRIGHT
+
+Copyright (c) 2004, chromatic.  All rights reserved.  This module is
+distributed under the same terms as Perl itself, in the hope that it is useful
+but certainly under no guarantee.
---
+++ t/boolean.t
@@ -0,0 +1,45 @@
+#!/usr/bin/perl -wT
+
+BEGIN
+{
+	chdir 't' if -d 't';
+	use lib '../lib', '../blib/lib';
+}
+
+use strict;
+
+use Test::More tests => 13;
+
+use Scalar::Util 'tainted';
+use Test::CGI::Untaint;
+
+my $module = 'CGI::Untaint::boolean';
+my $parent = 'CGI::Untaint::object' ;
+
+use_ok( $module ) or exit;
+ok( $module->isa( $parent ), "$module should extend $parent" );
+
+can_ok( $module, '_new' );
+
+my $bool = $module->_new( {} );
+isa_ok( $bool, $module );
+
+is_extractable( 'on',   1, 'boolean' );
+is_extractable(   '',  '', 'boolean' );
+
+unextractable( 'wibbly',  'boolean' );
+
+my $tainted_on = substr( 'on' .  $ENV{PATH}, 0, 2 );
+my $on         = $bool->_untaint_re( $tainted_on );
+ok(   tainted( $tainted_on ), 'insecure strings should be tainted (sanity)' );
+ok( ! tainted( $on ), '_untaint_re() should untaint them' );
+
+my $tainted_off = substr( 'off' .  $ENV{PATH}, 0, 3 );
+my $off         = $bool->_untaint_re( $tainted_off );
+ok( ! tainted( $off ), '... for both allowed checkbox values' );
+
+can_ok( $bool, 'is_valid' );
+$bool->value( 'on'  );
+ok(   $bool->is_valid(), "is_valid() should return true if value is 'on'" );
+$bool->value( 'foo' );
+ok( ! $bool->is_valid(), '... or false otherwise' );
